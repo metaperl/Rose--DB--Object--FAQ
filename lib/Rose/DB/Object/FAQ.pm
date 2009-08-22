2718476c81e869a2e8d15eebb338e3cc7e74caaf
Terrence Brannon <TBrannon@SmartFinancial.com>
2009-08-22 08:16:19 -0400

    breaking pont

--- lib/Rose/DB/Object/FAQ.pm
+++ lib/Rose/DB/Object/FAQ.pm
@@ -14,7 +14,7 @@ Rose::DB::Object::FAQ - FAQ for the Perl ORM Rose::DB::Object
 
 Terrence Brannon, C<< <metaperl at gmail.com> >>
 
-=head1 Meta Questions
+=head1 Programming Questions
 
 =head2 How to see what entire SQL I have produced
 
@@ -25,12 +25,63 @@ Terrence Brannon, C<< <metaperl at gmail.com> >>
 
 L<http://groups.google.com/group/rose-db-object/browse_thread/thread/ad0ebc1e8af314ce#>
 
+=head2 How to augment a table class with methods without losing them on re-gen?
+
+L<http://groups.google.com/group/rose-db-object/browse_thread/thread/f57d435284aa9a84#>
+
+... use module_postamble to "require
+My::DB::Whatever::Extra" (or some similarly named module) at the end
+of My/DB/Whatever.pm, inside which you'd add methods and possibly even
+tweak metadata and re-initialize( C<< replace_existing => 1>> ).  That way you
+can regenerate the modules without fear of touching any of your custom
+code. 
+
+=head2 How to use your own $dbh factory?
+
+=head2 Query Logging
+
+L<http://groups.google.com/group/rose-db-object/browse_thread/thread/77a77ffb0753ba4b#>
+
+
 =head1 SQL Generation
 
 =head2 How to get literal SQL
 
 It's documented here:
 L<http://search.cpan.org/dist/Rose-DB-Object/lib/Rose/DB/Object/QueryBuilder.pm#query> 
+
+=head2 Correlated subqueries
+
+=head3 example
+
+L<http://groups.google.com/group/rose-db-object/browse_thread/thread/977fe1f6a92e2e4d#>
+
+Let's generate
+
+  SELECT * FROM prices WHERE NOT EXISTS (the same product with a lower price) 
+  SELECT * FROM prices WHERE NOT EXISTS (SELECT * FROM prices p WHERE p.product_id = t1.product_id AND p.price < t1.price)
+
+  $prices =
+      Rose::DB::Object::Manager->get_objects(
+        object_class => 'Prices',
+        clauses =>
+        [
+          'NOT EXISTS (SELECT * FROM prices p WHERE ' .
+          'p.product_id = t1.product_id AND p.price < t1.price)',
+        ]); 
+
+Though personally I would just do:
+
+   SELECT price_id, product_id, MIN(price)
+   FROM prices 
+   GROUP BY product_id
+   ORDER BY price
+
+=head1 Optimization
+
+=head2 Reusing database handles
+
+L<http://groups.google.com/group/rose-db-object/browse_thread/thread/ae0c1f2b36c7d02f#>
 
 
 
